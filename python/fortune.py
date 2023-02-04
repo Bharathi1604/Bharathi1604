baf6042a4681d6dcb876531397049259561e41a3
Bharathi1604 <103473188+Bharathi1604@users.noreply.github.com>
2023-02-04 19:48:11 +0530

    Add files via upload

---
+++ python/fortune.py
@@ -0,0 +1,17 @@
+import random 
+
+def fortune_cookies():
+       a = random.randint(1,3)
+
+       if  a == 1:
+          print("Today will be a lucky day")
+       
+       elif a == 2:
+            print("you will meet someone and intresting")
+
+       elif a == 3:
+            print("watch out for obstacles in your path")
+
+fortune_cookies()
+
+
